867a24539fb89458b010ef81f78ef79dd49ef446
Afbomann <178702842+Afbomann@users.noreply.github.com>
2024-10-24 10:40:01 +0200

    Update page.tsx

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -42,6 +42,7 @@ export default async function BusDeparturePage({
             const dateNow = new Date();
 
             if (process.env.NODE_ENV == "production") {
+              estimatedCallDate.setHours(estimatedCallDate.getHours() + 2);
               dateNow.setHours(dateNow.getHours() + 2);
             }
 
